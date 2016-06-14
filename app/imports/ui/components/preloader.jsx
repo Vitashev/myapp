a007635716e34d5fa17cfc3563110b2b0f2c8a70
Vitashev <vitashev@bk.ru>
2016-06-14 21:27:56 +0300

    bla

---
+++ app/imports/ui/components/preloader.jsx
@@ -0,0 +1,20 @@
+import React, {Component, propTypes} from 'react';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+
+export default class Preloader extends TrackerReact(Component) {
+  render() {
+    return (
+      <div className="preloader-wrapper big active">
+      <div className="spinner-layer spinner-teal-only">
+        <div className="circle-clipper left">
+          <div className="circle"></div>
+        </div><div className="gap-patch">
+        <div className="circle"></div>
+      </div><div className="circle-clipper right">
+        <div className="circle"></div>
+      </div>
+      </div>
+    </div>
+    )
+  }
+}
